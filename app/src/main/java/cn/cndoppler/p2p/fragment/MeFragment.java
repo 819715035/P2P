fc842f98a2c9ca588151abe17b15176d1191a57a
lan <lan>
2017-11-06 14:20:50 +0800

    实现个人中心

--- app/src/main/java/cn/cndoppler/p2p/fragment/MeFragment.java
+++ app/src/main/java/cn/cndoppler/p2p/fragment/MeFragment.java
@@ -1,15 +1,30 @@
 package cn.cndoppler.p2p.fragment;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.loopj.android.http.RequestParams;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.cndoppler.p2p.R;
+import cn.cndoppler.p2p.activity.LoginActivity;
+import cn.cndoppler.p2p.bean.User;
+import cn.cndoppler.p2p.common.BaseActivity;
 import cn.cndoppler.p2p.common.BaseFragment;
 
 /**
@@ -18,13 +33,71 @@ import cn.cndoppler.p2p.common.BaseFragment;
 public class MeFragment extends BaseFragment {
 
 
+    @BindView(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @BindView(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @BindView(R.id.tv_me_name)
+    TextView tvMeName;
+    @BindView(R.id.rl_me)
+    RelativeLayout rlMe;
+    @BindView(R.id.recharge)
+    ImageView recharge;
+    @BindView(R.id.withdraw)
+    ImageView withdraw;
+    @BindView(R.id.ll_touzi)
+    TextView llTouzi;
+    @BindView(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @BindView(R.id.ll_zichan)
+    TextView llZichan;
+    Unbinder unbinder;
+
     public MeFragment() {
-        // Required empty public constructor
     }
 
     @Override
     protected void initData(String content) {
+        //判断用户是否已经登录
+        isLogin();
+    }
 
+    private void isLogin() {
+        //查看本地是否有用户的登录信息
+        SharedPreferences sp = this.getActivity().getSharedPreferences("user_info", Context.MODE_PRIVATE);
+        String name = sp.getString("name", "");
+        if (TextUtils.isEmpty(name)) {
+            //本地没有保存过用户信息，给出提示：登录
+            doLogin();
+        } else {
+            //已经登录过，则直接加载用户的信息并显示
+            doUser();
+        }
+    }
+
+    //加载用户信息并显示
+    private void doUser() {
+        //1.读取本地保存的用户信息
+        User user = ((BaseActivity) this.getActivity()).readUser();
+        //2.获取对象信息，并设置给相应的视图显示。
+        tvMeName.setText(user.getName());
+        Glide.with(this.getActivity()).load(user.getImageurl()).into(ivMeIcon);
+    }
+
+    //给出提示：登录
+    private void doLogin() {
+        new AlertDialog.Builder(this.getActivity())
+                .setTitle("提示")
+                .setMessage("您还没有登录哦！么么~")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                            UIUtils.toast("进入登录页面",false);
+                        ((BaseActivity) (MeFragment.this.getActivity())).openActivity(LoginActivity.class);
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 
     @Override
@@ -40,5 +113,19 @@ public class MeFragment extends BaseFragment {
     @Override
     public int getLayout() {
         return R.layout.fragment_me;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
